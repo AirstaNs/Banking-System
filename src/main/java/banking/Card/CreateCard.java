cd4a3973a7bfce56ca807a3f9b21d788443e10d2
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-10 07:38:56 +0500

    Passing the format to the constructor When creating a pincode and a card

--- src/main/java/banking/Card/CreateCard.java
+++ src/main/java/banking/Card/CreateCard.java
@@ -1,6 +1,14 @@
 package banking.Card;
 
+import banking.Formatted.Formatted;
+
 public class CreateCard implements ShouldBeGenerateCardNumber {
+    private Formatted format;
+
+    public CreateCard(Formatted format) {
+        this.format = format;
+    }
+
     @Override
     public String generated(int UserId) {
         return null;
--- src/main/java/banking/Pincode/CreatePin.java
+++ src/main/java/banking/Pincode/CreatePin.java
@@ -1,20 +1,20 @@
 package banking.Pincode;
 
-import banking.Pincode.FormattedPin.FormattedPin;
+import banking.Formatted.Formatted;
 import banking.RandomGenerate.GenerateSecureRandomInt;
 
 public class CreatePin  {
     private static final int MAX_VALUE_PIN = 10000;
     private static final int MIN_VALUE_PIN = 0;
 
-    private FormattedPin formatPin;
+    private Formatted formatPin;
 
-    public CreatePin(FormattedPin format) {
+    public CreatePin(Formatted format) {
         this.formatPin = format;
     }
 
     public String create() {
         int gen = GenerateSecureRandomInt.getInstance().generate(MAX_VALUE_PIN, MIN_VALUE_PIN);
-        return formatPin.castToNumbers(gen);
+        return formatPin.castToFormat(gen);
     }
 }
