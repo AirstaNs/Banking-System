0f784d1f05a485c81676e7440e11fb8a179b9747
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-11 08:20:02 +0500

    rename class Card, Pin

--- src/main/java/banking/Formatters/FormatterCard.java
+++
@@ -1,8 +0,0 @@
-package banking.Formatters;
-
-public class FormatterCard implements Formatter {
-    @Override
-    public String castToFormat(int value) {
-        return String.format("%09d", value);
-    }
-}
