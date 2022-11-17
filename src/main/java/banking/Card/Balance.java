ab40f4bffb68ab45402a72148e3b5cd710084eb8
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 12:32:32 +0500

    Renaming variables and methods, implementing toString and add comments in Balance

--- src/main/java/banking/Card/Balance.java
+++ src/main/java/banking/Card/Balance.java
@@ -2,37 +2,65 @@ package banking.Card;
 
 import java.util.Objects;
 
+/**
+ * The class describing the specific balance of the transferred {@link Card}. <br>
+ * When created, the balance is set by default = 0L. <br>
+ * In the {@link Card} class, the balance for the strong link is also passed.
+ */
 public class Balance {
-    private  Long value;
+    /**
+     * A variable that stores funds on the client's {@link Card}.
+     */
+    private Long balance;
+    /**
+     * The {@link Card} that stores this balance.
+     */
     private final Card card;
-    public Balance(Card card){
-        value = 0L;
+
+    public Balance(Card card) {
+        balance = 0L;
         this.card = card;
     }
 
-    public void increase(Long value){
-        this.value+=value;
+    /**
+     * Method to increase the balance on the card
+     *
+     * @param amount the value by which to increase the balance.
+     */
+    protected void deposit(Long amount) {
+        this.balance += amount;
     }
 
-    public void decrease(Long value){
-        this.value-=value;
+    /**
+     * Method to reduce the balance on the card.
+     *
+     * @param amount the value by which to reduce the balance.
+     */
+    protected void withdraw(Long amount) {
+        this.balance -= amount;
     }
-    public long getValue() {
-        return value;
+
+    protected long getBalance() {
+        return balance;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Balance)) return false;
-        if (this.hashCode()!=o.hashCode()) return false;
+        if (this.hashCode() != o.hashCode()) return false;
         Balance balance = (Balance) o;
 
-        return card.equals(balance.card) && value.equals(balance.value);
+        return card.equals(balance.card) && this.balance.equals(balance.balance);
     }
 
     @Override
     public int hashCode() {
-        return card != null ?  Objects.hash(card,value) : 0;
+        return card != null ? Objects.hash(card, balance) : 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Balance: %d%n", balance);
     }
 }
