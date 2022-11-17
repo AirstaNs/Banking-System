617ab13869061a498b8ddd02dcb582088a936862
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 11:41:46 +0500

    Create a class Page that provides pages for menus in the console.

---
+++ src/main/java/banking/TestCommand/Page.java
@@ -0,0 +1,38 @@
+package banking.TestCommand;
+
+import banking.TestCommand.Actions.*;
+import banking.TestCommand.Recivers.LoginMenu;
+import banking.TestCommand.Recivers.PersonalMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class that provides pages for menus in the console.
+ */
+public class Page {
+    /**
+     * @param loginMenu    {@link LoginMenu}
+     * @param personalMenu {@link PersonalMenu}
+     * @return Returns "Page" - a list of home page menu actions.
+     */
+    public static List<Action> welcomePage(LoginMenu loginMenu, PersonalMenu personalMenu) {
+        return new ArrayList<>() {{
+            add(new Exit_Action(this.size(), loginMenu));
+            add(new CreateAccount_Action(this.size(), loginMenu));
+            add(new LogInAccount_Action(this.size(), loginMenu, personalMenu));
+        }};
+    }
+
+    /**
+     * @param personalMenu {@link PersonalMenu}
+     * @return Returns "Page" - a list of Personal account page menu actions.
+     */
+    public static List<Action> personalPage(PersonalMenu personalMenu) {
+        return new ArrayList<>() {{
+            add(new Exit_Action(this.size(), personalMenu));
+            add(new Balance_Action(this.size(), personalMenu));
+            add(new LogOutAccount_Action(this.size(), personalMenu));
+        }};
+    }
+}
