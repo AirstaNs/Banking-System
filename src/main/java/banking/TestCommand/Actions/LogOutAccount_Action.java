7cd9bfcc08a67b941b8e46542b42c7fb0383ccd4
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 10:31:34 +0500

    create concrete Action "Log Out Account" in menu item for Personal Menu

---
+++ src/main/java/banking/TestCommand/Actions/LogOutAccount_Action.java
@@ -0,0 +1,21 @@
+package banking.TestCommand.Actions;
+
+import banking.TestCommand.Recivers.PersonalMenu;
+
+public class LogOutAccount_Action extends Action {
+
+    private static final String title = "Log out";
+    private final PersonalMenu personalMenu;
+
+
+    public LogOutAccount_Action(int numberItem, PersonalMenu personalMenu) {
+        super(title, numberItem);
+        this.personalMenu = personalMenu;
+    }
+
+
+    @Override
+    public void execute() {
+        personalMenu.LogOut();
+    }
+}
