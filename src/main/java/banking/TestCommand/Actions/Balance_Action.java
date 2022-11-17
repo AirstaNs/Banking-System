b1eb9ead4e96f7903df6d4fb6e9c9c1eb696b0f5
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 10:35:40 +0500

    create concrete Action "Balance" in menu item for PersonalMenu

---
+++ src/main/java/banking/TestCommand/Actions/Balance_Action.java
@@ -0,0 +1,21 @@
+package banking.TestCommand.Actions;
+
+
+import banking.TestCommand.Recivers.PersonalMenu;
+
+public class Balance_Action extends Action {
+
+    private static final String title = "Balance";
+
+    private final PersonalMenu personalMenu;
+
+    public Balance_Action(int numberItem, PersonalMenu personalMenu) {
+        super(title, numberItem);
+        this.personalMenu = personalMenu;
+    }
+
+    @Override
+    public void execute() {
+        personalMenu.balance();
+    }
+}
