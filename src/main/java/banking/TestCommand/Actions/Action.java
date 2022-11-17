526fb6091e071663d86e9772b2143e46d0252103
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 10:30:04 +0500

    create an abstract class Action for menu items

---
+++ src/main/java/banking/TestCommand/Actions/Action.java
@@ -0,0 +1,28 @@
+package banking.TestCommand.Actions;
+
+// COMMAND
+public abstract class Action {
+    private final String title;
+    private final int numberItem;
+
+    public Action(String title, int numberItem) {
+        this.title = title;
+        this.numberItem = numberItem;
+    }
+
+    public abstract void execute();
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumberItem() {
+        return numberItem;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s", getNumberItem(), getTitle());
+    }
+}
