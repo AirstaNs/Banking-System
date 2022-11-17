bb1d0ec800967217372bd48c8f19dfff4c8afa9e
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-17 11:21:28 +0500

    Creation of the User's Personal Account page.

---
+++ src/main/java/banking/TestCommand/Recivers/PersonalMenu.java
@@ -0,0 +1,53 @@
+package banking.TestCommand.Recivers;
+
+import banking.TestCommand.Controller;
+import banking.TestCommand.HomeAutomationDemo;
+import banking.TestCommand.Page;
+import banking.client.User;
+
+/**
+ * RECEIVER  in Command pattern
+ * <br>
+ * The personal account page in the system is available after logging in.
+ * A specific menu item and page, each method describes a specific action on the page.
+ */
+public class PersonalMenu implements ShouldBeExit {
+    /**
+     * Personal account of users, detection action for it.
+     */
+    private User user;
+    /**
+     * Needed to change the displayed page in the console
+     */
+    private final Controller controller;
+
+    public PersonalMenu(Controller controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * Set the user for this page.
+     *
+     * @param user Personal account of users, detection action for it.
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Action to display the balance on the console for a specific user
+     */
+    public void balance() {
+        System.out.println(user.getCard().getBalance()); //FIXME
+    }
+
+    /**
+     * Exit from the Personal Account. <br>
+     * Removing the user from the page, Redirecting to the login page.
+     */
+    public void LogOut() {
+        System.out.println("You have successfully logged out!\n");
+        user = null;
+        controller.setPage(Page.welcomePage(HomeAutomationDemo.loginMenu, HomeAutomationDemo.personalMenu));
+    }
+}
