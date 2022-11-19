258364fc4d6221034f0ed0b52b44e0f8d089a60f
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-19 22:22:52 +0500

    The "Luna Algorithm" class is divided into methods. <br> Now the card number is set in the constructor.

--- src/main/java/banking/Card/AlgorithmLuna.java
+++ src/main/java/banking/Card/AlgorithmLuna.java
@@ -1,65 +1,94 @@
 package banking.Card;
 
-
 import java.util.function.Function;
 import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
+import banking.Utils;
+
 public class AlgorithmLuna {
+    /**
+     * If the number is greater than 9 then add up its digits. <br>
+     * Example: 18 -> 9
+     */
+    private static final IntUnaryOperator sumDigitsNumber = n -> n / 10 + n % 10;
+    /**
+     * Set when initializing the card number
+     */
+    private Function<Integer, Boolean> alternate;
+    private String card;
 
-
-    public static void main(String[] args) {
-        String card = "135138954993914435";
+    public AlgorithmLuna(String card) {
+        this.card = card;
+        var isEvenLength = Utils.isEven(card.length());
+        setIndexesExecuteAlgorithm(isEvenLength);
     }
 
+    /**
+     * Get checksum digit
+     *
+     * @return checksum digit
+     */
+    public int getNextNumberCardFormatLuna() {
+        int[] charsCard = Utils.StringToNumberArray(card); // Expand the card number, start from the end.
+        int sum = getSumFromCardNumber(charsCard);
+        return getMissingValueMultipleTen(sum);
 
-    public static String getLastNumberCardFormatLuna(String card) {
-        int[] charsCard = StringToIntArray(card);
-        var isEvenLength = charsCard.length % 2 == 0;
-        Function<Integer, Boolean> br1 = index -> (isEvenLength ? (index & 1) != 0 : (index % 2 == 0));
-
-        /*
-               Если length четное то false , если нечетное то true
-                */
-
-        IntUnaryOperator sumDigits = n -> n / 10 + n % 10;
-        IntUnaryOperator multiplyTwo = digit -> digit * 2;
-
-
-        int sum = IntStream.range(0, card.length())
-                           .map(i -> br1.apply(i) ? multiplyTwo.applyAsInt(charsCard[i]) : charsCard[i])
-                           .map(sumDigits)
-                           .sum();
-        return (10 - sum % 10) % 10 + "";
     }
 
-
-    //    public static int calculateLuhnsCheckDigit(String number) {
-    //        int     sum       = 0;
-    //        boolean alternate = false;
-    //
-    //        for (int i = number.length() - 1; i >= 0; --i) {
-    //            int digit = Character.getNumericValue(number.charAt(i)); // get the digit at the given index
-    //            System.out.println(i);
-    //            System.out.println(i+" "+alternate);
-    //            alternate = !alternate;
-    //            digit = (alternate) ? (digit * 2) : digit;  // double every other digit
-    //            digit = (digit >= 10) ? (digit / 10 + digit % 10) : digit;  // subtract 9 if the value is greater than 9
-    //            sum += digit;                                            // add the digit to the sum
-    //        }
-    //
-    //        return (10 - sum % 10) % 10;
-    //        //return (10 - sum % 10) % 10;
-    //    }
-
-    public static boolean isFormattedLuna(String numberCard) {
-        //   if(numberCard.length()!=CardFormat.LENGTH_CARD_NUMBER) return false;
-        return numberCard.matches("^\\d{" + CardFormat.LENGTH_CARD_NUMBER + "}$");
+    public boolean isFormattedLuna() {
+        var lenth = card.matches("^\\d{" + DebitCardFormat.LENGTH_CARD_NUMBER + "}$");
+        return lenth & getSumFromCardNumber(Utils.StringToNumberArray(card)) % 10 == 0;
     }
 
-    private static int[] StringToIntArray(String str) {
-        return str.chars()
-                  .map(Character::getNumericValue)
-                  .toArray();
+    /**
+     * Get the sum using the Luna algorithm.
+     *
+     * @param charsCard Array of card digits
+     * @return sum according to the Luna algorithm
+     */
+    public int getSumFromCardNumber(int[] charsCard) {
+        return IntStream.range(0, charsCard.length)
+                        .map(i -> alternate.apply(i) ? multiplyTwo(charsCard[i]) : charsCard[i])
+                        .map(sumDigitsNumber)
+                        .sum();
     }
+
+    /**
+     * Specify which indexes should be multiplied by two. <br>
+     * Is set in the constructor. <br>
+     *
+     * @param isEvenLength Depends on card length. <br> If the length of the map is odd, then we go to odd indices and vice versa.
+     */
+    private void setIndexesExecuteAlgorithm(boolean isEvenLength) {
+        if (isEvenLength) {
+            alternate = Utils::isOdd;
+        } else {
+            alternate = Utils::isEven;
+        }
+    }
+
+    /**
+     * We multiply the numbers by two if they are in even / odd places. <br>
+     * Depends on the parity of the length
+     *
+     * @param digit digit card
+     * @return digit card  multiply Two
+     */
+    private static int multiplyTwo(int digit) {
+        return digit << 1;
+    }
+
+    /**
+     * Get the digit missing for parity from the sum. <br>
+     * If the sum = 18 - the missing digit is 2. return 2 <br>
+     * If the sum is a multiple of 10, then we extract 0. The sequence is correct.
+     *
+     * @param sum the amount obtained by applying the previous steps of the Luna algorithm.
+     * @return number that is missing.
+     */
+    private int getMissingValueMultipleTen(int sum) {
+        return (10 - sum % 10) % 10;
+    }
+
 }
