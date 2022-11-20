238b680f4659628b2a55b36636bcc27c146b882b
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-20 23:18:39 +0500

    rename CardFormat.java to DebitCardFormat

--- src/main/java/banking/Card/CardFormat.java
+++
@@ -1,9 +0,0 @@
-package banking.Card;
-
- interface CardFormat {
-     String BIN = "400000";
-     int LENGTH_CARD_NUMBER = 16;
-
-     String generated(int UserId);
- }
-//ShouldBeGenerateCardNumber
