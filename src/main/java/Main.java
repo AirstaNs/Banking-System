49b05053a57e362ad4ff9e330087a1c385720002
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-24 03:51:30 +0500

    Extract the database name from the program arguments when starting the program and passing it to the system.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import banking.MenuActions.BankSystem;
 
 public class Main {
-
+    /**
+     * the database name is extracted from the program arguments. <br>
+     * Example = -fileName test.db
+     */
     public static void main(String[] args) {
         BankSystem system = new BankSystem();
-        system.start();
+        String nameDB = args[1];
+        system.start(nameDB);
     }
 }
