d2924b2797f21d9e0bc8a00ab3acfec9889d433c
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-11-24 03:32:41 +0500

    Adding getCountUser method and interface description

---
+++ src/main/java/banking/DAO/Context.java
@@ -0,0 +1,30 @@
+package banking.DAO;
+
+import banking.Card.Card;
+import banking.client.User;
+
+import java.util.Optional;
+
+/**
+ * An interface to provide multiple repository implementations for storing data about a system user.
+ */
+public interface Context {
+
+    int addUser(User user);
+
+    boolean removeUser(User user);
+
+    boolean containsUser(User user);
+
+    boolean containsUser(Card cardUser);
+
+    boolean containsUser(String number, String pin);
+
+
+    Optional<User> getUser(String number, String pin);
+
+    Optional<User> getUser(Card card);
+
+    int getCountUser();
+
+}
